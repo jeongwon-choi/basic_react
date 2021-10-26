7a25cac3b139cce33a22bec95af0194be6dabad7
jeongwon-choi <chol0501@nate.com>
2021-10-26 19:16:15 +0900

    처음 셋팅 저장

---
+++ index.js
@@ -0,0 +1,9 @@
+//백엔드 시작점 라우터 만들기
+
+const express = require('express')
+const app = express()
+const port = 5000
+
+app.get('/', (req, res) => {res.send('Hello World! 안녕하세요 ~! ')})
+
+app.listen(port, () => {console.log(`Example app listening at http://localhost:${port}`)})
